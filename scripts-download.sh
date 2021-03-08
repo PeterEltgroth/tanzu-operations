ff0db40d5910466d1b4c76dbf3bcc6162505eab5
nycpivot <mijames@vmware.com>
2021-03-08 16:19:41 -0500

    Ongoing changes.

--- scripts-download.sh
+++ scripts-download.sh
@@ -3,9 +3,9 @@ wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-a
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-build-service/tbs-on-tkg-with-gitlab-registry.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-build-service/tbs-on-tkg-with-harbor-registry.sh
 
-mkdir scripts
+mkdir operations
 
-mv base-setup.sh scripts/base-setup.sh
-mv tkg-aws-management-cluster-setup.sh scripts/tkg-aws-management-cluster-setup.sh
-mv tbs-on-tkg-with-gitlab-registry.sh scripts/tbs-on-tkg-with-gitlab-registry.sh
-mv tbs-on-tkg-with-harbor-registry.sh scripts/tbs-on-tkg-with-harbor-registry.sh
+mv base-setup.sh operations/base-setup.sh
+mv tkg-aws-management-cluster-setup.sh operations/tkg-aws-management-cluster-setup.sh
+mv tbs-on-tkg-with-gitlab-registry.sh operations/tbs-on-tkg-with-gitlab-registry.sh
+mv tbs-on-tkg-with-harbor-registry.sh operations/tbs-on-tkg-with-harbor-registry.sh
