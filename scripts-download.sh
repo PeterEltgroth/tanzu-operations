2b702b0e9b88aab71b64147fbe76b0fc4d15de09
nycpivot <mijames@vmware.com>
2021-03-08 16:31:19 -0500

    Ongoing changes.

--- scripts-download.sh
+++ scripts-download.sh
@@ -3,9 +3,9 @@ wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-a
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-build-service/tbs-on-tkg-with-gitlab-registry.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-build-service/tbs-on-tkg-with-harbor-registry.sh
 
-mkdir operations
+mkdir operations-setup
 
-mv base-setup.sh operations/base-setup.sh
-mv tkg-aws-management-cluster-setup.sh operations/tkg-aws-management-cluster-setup.sh
-mv tbs-on-tkg-with-gitlab-registry.sh operations/tbs-on-tkg-with-gitlab-registry.sh
-mv tbs-on-tkg-with-harbor-registry.sh operations/tbs-on-tkg-with-harbor-registry.sh
+mv base-setup.sh operations-setup/base-setup.sh
+mv tkg-aws-management-cluster-setup.sh operations-setup/tkg-aws-management-cluster-setup.sh
+mv tbs-on-tkg-with-gitlab-registry.sh operations-setup/tbs-on-tkg-with-gitlab-registry.sh
+mv tbs-on-tkg-with-harbor-registry.sh operations-setup/tbs-on-tkg-with-harbor-registry.sh
--- scripts-initialize.sh
+++ scripts-initialize.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/linux-post-script.sh
-mv linux-post-script.sh ~/linux-post-script.sh
-sh ~/linux-post-script.sh
-rm ~/linux-post-script.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/scripts-download.sh
+mv scripts-download.sh ~/scripts-download.sh
+sh ~/scripts-download.sh
+rm ~/scripts-download.sh
