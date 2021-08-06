1f79909e265eb39193e75a3c79e4bad02f122c72
nycpivot <mijames@vmware.com>
2021-08-06 14:59:11 -0400

    Ongoing changes.

--- tanzu-on-aws/tanzu-kubernetes-grid/tkg-cluster-aws-setup.sh
+++ tanzu-on-aws/tanzu-kubernetes-grid/tkg-cluster-aws-setup.sh
@@ -9,7 +9,7 @@ tanzu login
 
 read -p "Cluster 1: " cluster1
 
-tanzu cluster $cluster1 -f .tanzu/tkg/clusterconfigs/$new_filename --plan dev
+tanzu cluster create $cluster1 -f .tanzu/tkg/clusterconfigs/$new_filename --plan dev
 tanzu cluster kubeconfig get $cluster1 --admin
 
 read -p "Cluster 2: " cluster2
