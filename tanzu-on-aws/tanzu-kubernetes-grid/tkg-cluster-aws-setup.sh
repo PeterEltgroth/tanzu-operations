1712f60fac70a6cf6b751096e383d770c2445eac
nycpivot <mijames@vmware.com>
2021-08-06 13:33:26 -0400

    Ongoing changes.

--- tanzu-on-aws/tanzu-kubernetes-grid/tkg-cluster-aws-setup.sh
+++ tanzu-on-aws/tanzu-kubernetes-grid/tkg-cluster-aws-setup.sh
@@ -1,12 +1,19 @@
 ls .tanzu/tkg/clusterconfigs
 
-read -p "Management Cluster Config: " filename
+read -p "Cluster Config: " filename
+read -p "New Cluster Config: " new_filename
 
-rm .tanzu/tkg/clusterconfigs/aws-config.yaml
-mv .tanzu/tkg/clusterconfigs/$filename .tanzu/tkg/clusterconfigs/aws-config.yaml
+rm .tanzu/tkg/clusterconfigs/$new_filename
+mv .tanzu/tkg/clusterconfigs/$filename .tanzu/tkg/clusterconfigs/$new_filename
 
-tanzu cluster create tanzu-aws-nginx-dev -f .tanzu/tkg/clusterconfigs/aws-config.yaml --plan dev
-tanzu cluster kubeconfig get tanzu-aws-nginx-dev --admin
+tanzu login
 
-tanzu cluster create tanzu-aws-nginx-prod -f .tanzu/tkg/clusterconfigs/aws-config.yaml --plan dev
-tanzu cluster kubeconfig get tanzu-aws-nginx-prod --admin
+read -p "Cluster 1: " cluster1
+
+tanzu cluster $cluster1 -f .tanzu/tkg/clusterconfigs/$new_filename --plan dev
+tanzu cluster kubeconfig get $cluster1 --admin
+
+read -p "Cluster 2: " cluster2
+
+tanzu cluster create $cluster2 -f .tanzu/tkg/clusterconfigs/$new_filename --plan dev
+tanzu cluster kubeconfig get $cluster2 --admin
