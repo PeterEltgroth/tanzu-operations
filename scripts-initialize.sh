2c506fd2cadb6655261b0972d24a2506be274034
nycpivot <mijames@vmware.com>
2021-08-06 21:22:06 -0400

    Renamed files.

--- scripts-initialize.sh
+++ scripts-initialize.sh
@@ -1,4 +1,3 @@
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/scripts-download.sh
-mv scripts-download.sh ~/scripts-download.sh
 sh ~/scripts-download.sh
 rm ~/scripts-download.sh
