cbeb36bbd8d2ac7826abfd2b3c9cb97de56f7b3c
nycpivot <mijames@vmware.com>
2021-08-08 09:31:22 -0400

    More changes.

--- scripts-download.sh
+++ scripts-download.sh
@@ -21,3 +21,24 @@ mv tkg-cluster-aws-setup.sh operations-setup/tkg-cluster-aws-setup.sh
 mv tbs-on-tkg-with-gitlab-registry.sh operations-setup/tbs-on-tkg-with-gitlab-registry.sh
 mv tbs-on-tkg-with-harbor-registry.sh operations-setup/tbs-on-tkg-with-harbor-registry.sh
 mv tmc-aws-setup.sh operations-setup/tmc-aws-setup.sh
+
+
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/demo-live.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/namespace-spring-app.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/namespace-spring-data.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/namespace-spring-web.yaml
+
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/policies/network-database-policy.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/policies/quota-development-policy.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/policies/registry-policy.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/policies/security-production-policy.yaml
+
+mkdir tmc-policies
+
+mv namespace-spring-app.yaml tmc-policies/namespace-spring-app.yaml
+mv namespace-spring-data.yaml tmc-policies/namespace-spring-data.yaml
+mv namespace-spring-web.yaml tmc-policies/namespace-spring-web.yaml
+mv network-database-policy.yaml tmc-policies/network-database-policy.yaml
+mv quota-development-policy.yaml tmc-policies/quota-development-policy.yaml
+mv registry-policy.yaml tmc-policies/registry-policy.yaml
+mv security-production-policy.yaml tmc-policies/security-production-policy.yaml
--- tanzu-on-aws/tanzu-mission-control/tmc-aws-setup.sh
+++ tanzu-on-aws/tanzu-mission-control/tmc-aws-setup.sh
@@ -1,6 +1,10 @@
 #ATTACH PROD CLUSTER
-#rm ./k8s-attach-manifest.yaml
-#tmc cluster attach --name tanzu-aws-nginx-prod --cluster-group production
+rm ./k8s-attach-manifest.yaml
+tmc cluster attach --name tanzu-cluster-nginx-prod-eu-west-1 --cluster-group production
 
-#kubectl config use-context tanzu-aws-nginx-prod-admin@tanzu-aws-nginx-prod
-#kubectl apply -f ./k8s-attach-manifest.yaml
+kubectl config use-context tanzu-cluster-nginx-prod-eu-west-1-admin@tanzu-cluster-nginx-prod-eu-west-1
+kubectl apply -f ./k8s-attach-manifest.yaml
+
+tmc cluster namespace create -f tmc-policies/namespace-spring-web-prod.yaml
+tmc cluster namespace create -f tmc-policies/namespace-spring-app-prod.yaml
+tmc cluster namespace create -f tmc-policies/namespace-spring-data-prod.yaml
