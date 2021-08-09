38aa2f9190a4c92e6ca7422fe7625bbf91d4adee
nycpivot <mijames@vmware.com>
2021-08-08 20:03:03 -0400

    Changed file references.

--- scripts-download.sh
+++ scripts-download.sh
@@ -1,51 +1,30 @@
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/base-setup.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tkg-setup.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tbs-setup.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-setup.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-kubernetes-grid/tkg-management-cluster-aws-setup.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-kubernetes-grid/tkg-management-cluster-aws-setup-ui.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-kubernetes-grid/tkg-cluster-aws-setup.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-build-service/tbs-on-tkg-with-gitlab-registry.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-build-service/tbs-on-tkg-with-harbor-registry.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-mission-control/tmc-aws-setup.sh
-
 mkdir operations-setup
+mkdir tmc
+mkdir deploy
 
-mv base-setup.sh operations-setup/base-setup.sh
-mv tkg-setup.sh operations-setup/tkg-setup.sh
-mv tbs-setup.sh operations-setup/tbs-setup.sh
-mv tmc-setup.sh operations-setup/tmc-setup.sh
-mv tkg-management-cluster-aws-setup.sh operations-setup/tkg-management-cluster-aws-setup.sh
-mv tkg-management-cluster-aws-setup-ui.sh operations-setup/tkg-management-cluster-aws-setup-ui.sh
-mv tkg-cluster-aws-setup.sh operations-setup/tkg-cluster-aws-setup.sh
-mv tbs-on-tkg-with-gitlab-registry.sh operations-setup/tbs-on-tkg-with-gitlab-registry.sh
-mv tbs-on-tkg-with-harbor-registry.sh operations-setup/tbs-on-tkg-with-harbor-registry.sh
-mv tmc-aws-setup.sh operations-setup/tmc-aws-setup.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/base-setup.sh -O operations-setup/base-setup.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tkg-setup.sh -O operations-setup/tkg-setup.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tbs-setup.sh -O operations-setup/tbs-setup.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-setup.sh -O operations-setup/tmc-setup.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-kubernetes-grid/tkg-management-cluster-aws-setup.sh -O operations-setup/tkg-management-cluster-aws-setup.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-kubernetes-grid/tkg-management-cluster-aws-setup-ui.sh -O operations-setup/tkg-management-cluster-aws-setup-ui.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-kubernetes-grid/tkg-cluster-aws-setup.sh -O operations-setup/tkg-cluster-aws-setup.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-build-service/tbs-on-tkg-with-gitlab-registry.sh -O operations-setup/tbs-on-tkg-with-gitlab-registry.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-build-service/tbs-on-tkg-with-harbor-registry.sh -O operations-setup/tbs-on-tkg-with-harbor-registry.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-on-aws/tanzu-mission-control/tmc-aws-setup.sh -O operations-setup/tmc-aws-setup.sh
+
 
 wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/reset.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/demo-live.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/namespace-spring-app-dev.yaml
-wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/namespace-spring-data-dev.yaml
-wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/namespace-spring-web-dev.yaml
-wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/namespace-spring-app-prod.yaml
-wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/namespace-spring-data-prod.yaml
-wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/namespace-spring-web-prod.yaml
-wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/deployment-spring-music-quota.yaml
 
-wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/policies/network-database-policy.yaml
-wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/policies/quota-development-policy.yaml
-wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/policies/registry-policy.yaml
-wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/policies/security-production-policy.yaml
-
-mkdir tmc-policies
-
-mv namespace-spring-app-dev.yaml tmc-policies/namespace-spring-app-dev.yaml
-mv namespace-spring-data-dev.yaml tmc-policies/namespace-spring-data-dev.yaml
-mv namespace-spring-web-dev.yaml tmc-policies/namespace-spring-web-dev.yaml
-mv namespace-spring-app-prod.yaml tmc-policies/namespace-spring-app-prod.yaml
-mv namespace-spring-data-prod.yaml tmc-policies/namespace-spring-data-prod.yaml
-mv namespace-spring-web-prod.yaml tmc-policies/namespace-spring-web-prod.yaml
-mv network-database-policy.yaml tmc-policies/network-database-policy.yaml
-mv quota-development-policy.yaml tmc-policies/quota-development-policy.yaml
-mv registry-policy.yaml tmc-policies/registry-policy.yaml
-mv security-production-policy.yaml tmc-policies/security-production-policy.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/tmc/namespace-spring-app-dev.yaml -O tmc/namespace-spring-app-dev.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/tmc/namespace-spring-data-dev.yaml -O tmc/namespace-spring-data-dev.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/tmc/namespace-spring-web-dev.yaml -O tmc/namespace-spring-web-dev.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/tmc/namespace-spring-app-prod.yaml -O tmc/namespace-spring-app-prod.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/tmc/namespace-spring-data-prod.yaml -O tmc/namespace-spring-data-prod.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/tmc/namespace-spring-web-prod.yaml -O tmc/namespace-spring-web-prod.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/tmc/network-database-policy.yaml -O tmc/network-database-policy.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/tmc/quota-development-policy.yaml -O tmc/quota-development-policy.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/tmc/registry-policy.yaml -O tmc/registry-policy.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/tmc/security-production-policy.yaml -O tmc/security-production-policy.yaml
+wget https://raw.githubusercontent.com/nycpivot/tanzu-amazon/main/tanzu-advanced-in-depth/deploy/deployment-spring-music-quota.yaml -O deploy/deployment-spring-music-quota.yaml
