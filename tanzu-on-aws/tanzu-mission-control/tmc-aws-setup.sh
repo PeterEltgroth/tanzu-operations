bdc92ed173de2b0d2438baa785494ec903752c37
nycpivot <mijames@vmware.com>
2021-08-08 21:24:06 -0400

    Changed file references.

--- tanzu-on-aws/tanzu-mission-control/tmc-aws-setup.sh
+++ tanzu-on-aws/tanzu-mission-control/tmc-aws-setup.sh
@@ -5,6 +5,6 @@ tmc cluster attach --name tanzu-cluster-nginx-prod-eu-west-1 --cluster-group pro
 kubectl config use-context tanzu-cluster-nginx-prod-eu-west-1-admin@tanzu-cluster-nginx-prod-eu-west-1
 kubectl apply -f ./k8s-attach-manifest.yaml
 
-tmc cluster namespace create -f tmc-policies/namespace-spring-web-prod.yaml
-tmc cluster namespace create -f tmc-policies/namespace-spring-app-prod.yaml
-tmc cluster namespace create -f tmc-policies/namespace-spring-data-prod.yaml
+tmc cluster namespace create -f tmc/namespace-spring-web-prod.yaml
+tmc cluster namespace create -f tmc/namespace-spring-app-prod.yaml
+tmc cluster namespace create -f tmc/namespace-spring-data-prod.yaml
