1c5e8079fc51b4d2401d0f80b0e95c4a5df4e055
nycpivot <mijames@vmware.com>
2022-01-12 22:36:21 -0500

    Modifications for version 1.0.

--- tanzu-application-platform/tanzu-application-operator-us-east-2.sh
+++ tanzu-application-platform/tanzu-application-operator-us-east-2.sh
@@ -1,1 +1,1 @@
-ssh ubuntu@ec2-3-145-121-193.us-east-2.compute.amazonaws.com -i ../keys/tanzu-application-operator-us-east-2.pem
+ssh ubuntu@ec2-18-118-109-36.us-east-2.compute.amazonaws.com -i ../keys/tanzu-application-operator-us-east-2.pem
