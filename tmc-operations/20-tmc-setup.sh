cb791b0f62b81f6dfa0820a28a5bfdbd6907b500
nycpivot <mijames@vmware.com>
2022-01-18 21:27:41 -0500

    Updated scripts to setup TMC prereqs.

--- tmc-operations/20-tmc-setup.sh
+++ tmc-operations/20-tmc-setup.sh
@@ -1,10 +1,10 @@
 read -p "Azure Subscription: " subscription
 
-access_cluster_group=tmc-access-cluster-group
-security_cluster_group=tmc-security-cluster-group
-quota_cluster_group=tmc-quota-cluster-group
-custom_cluster_group=tmc-custom-cluster-group
-registry_and_network_cluster=tmc-registry-and-network-cluster
+access_cluster_group=eks-access-cluster-group
+quota_cluster_group=eks-quota-cluster-group
+custom_cluster_group=eks-custom-cluster-group
+security_cluster_group=gke-security-cluster-group
+registry_and_network_cluster=aks-registry-and-network-cluster
 
 cluster_group=default-cluster-group
 workspace_group=default-workspace-group
@@ -18,44 +18,50 @@ echo $TMC_API_TOKEN
 tmc login
 
 #CREATE CLUSTER GROUPS
-#tmc clustergroup create --name $cluster_group --description "Demonstrates the cluster-only policies; security, quota, and custom."
+read -p "Create cluster groups"
+tmc clustergroup create --name $cluster_group --description "Demonstrates the cluster-only policies; security, quota, and custom."
 tmc clustergroup create --name $workspace_group --description "Demonstrates the workspace-only policies; image registry and network."
 
 
-#CREATE ACCESS CLUSTER
-#kubectl config use-context arn:aws:eks:ap-northeast-1:964978768106:cluster/${access_cluster_group}
+#CREATE EKS ACCESS CLUSTER
+read -p "Attach EKS Access Cluster"
+kubectl config use-context $access_cluster_group
 
-#rm ./k8s-attach-manifest.yaml
-#tmc cluster attach --name $access_cluster_group --cluster-group $cluster_group
-#kubectl apply -f ./k8s-attach-manifest.yaml
+rm ./k8s-attach-manifest.yaml
+tmc cluster attach --name $access_cluster_group --cluster-group $cluster_group
+kubectl apply -f ./k8s-attach-manifest.yaml
+
+
+#CREATE EKS QUOTA CLUSTER
+read -p "Attach EKS Quota Cluster"
+kubectl config use-context $quota_cluster_group
+
+rm ./k8s-attach-manifest.yaml
+tmc cluster attach --name $quota_cluster_group --cluster-group $cluster_group
+kubectl apply -f ./k8s-attach-manifest.yaml
+
+
+#CREATE EKS CUSTOM CLUSTER
+read -p "Attach EKS Custom Cluster"
+kubectl config use-context $custom_cluster_group
+
+rm ./k8s-attach-manifest.yaml
+tmc cluster attach --name $custom_cluster_group --cluster-group $cluster_group
+kubectl apply -f ./k8s-attach-manifest.yaml
 
 
 #CREATE SECURITY CLUSTER
-#kubectl config use-context arn:aws:eks:ap-northeast-1:964978768106:cluster/${security_cluster_group}
+read -p "Attach GKE Security Cluster"
+kubectl config use-context $security_cluster_group
 
-#rm ./k8s-attach-manifest.yaml
-#tmc cluster attach --name $security_cluster_group --cluster-group $cluster_group
-#kubectl apply -f ./k8s-attach-manifest.yaml
-
-
-#CREATE QUOTA CLUSTER
-#kubectl config use-context arn:aws:eks:ap-northeast-1:964978768106:cluster/${quota_cluster_group}
-
-#rm ./k8s-attach-manifest.yaml
-#tmc cluster attach --name $quota_cluster_group --cluster-group $cluster_group
-#kubectl apply -f ./k8s-attach-manifest.yaml
-
-
-#CREATE CUSTOM CLUSTER
-#kubectl config use-context arn:aws:eks:ap-northeast-1:964978768106:cluster/${custom_cluster_group}
-
-#rm ./k8s-attach-manifest.yaml
-#tmc cluster attach --name $custom_cluster_group --cluster-group $cluster_group
-#kubectl apply -f ./k8s-attach-manifest.yaml
+rm ./k8s-attach-manifest.yaml
+tmc cluster attach --name $security_cluster_group --cluster-group $cluster_group
+kubectl apply -f ./k8s-attach-manifest.yaml
 
 
 #CREATE REGISTRY AND NETWORK CLUSTER
-kubectl config use-context arn:aws:eks:ap-northeast-1:964978768106:cluster/${registry_and_network_cluster}
+read -p "Attach AKS Registry/Network Cluster"
+kubectl config use-context $registry_and_network_cluster
 
 rm ./k8s-attach-manifest.yaml
 tmc cluster attach --name $registry_and_network_cluster --cluster-group $workspace_group
