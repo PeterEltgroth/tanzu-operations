8f6b1fd92e9b059339053e6ca03c236f7f92a464
nycpivot <mijames@vmware.com>
2022-01-20 12:20:58 -0500

    Build and run tap values.

--- tanzu-application-platform/scripts-download.sh
+++ tanzu-application-platform/scripts-download.sh
@@ -9,7 +9,8 @@ wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-appl
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-application-platform/10-pre-reqs-us-west-1.sh -O tap/10-pre-reqs-us-west-1.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-application-platform/11-pre-reqs-tanzu.sh -O tap/11-pre-reqs-tanzu.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-application-platform/20-tap-packages.sh -O tap/20-tap-packages.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-application-platform/30-tap-gui.sh -O tap/30-tap-gui.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-application-platform/30-tap-values-build.sh -O tap/30-tap-values-build.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-application-platform/30-tap-values-run.sh -O tap/30-tap-values-run.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-application-platform/50-developer-namespace.sh -O tap/50-developer-namespace.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-application-platform/51-tilt.sh -O tap/51-tilt.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-application-platform/60-deploy-rabbitmq-operator.sh -O tap/60-deploy-rabbitmq-operator.sh
