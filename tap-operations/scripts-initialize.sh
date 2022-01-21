de9d13e6b98102da0ab624757ecd252c1282f2bf
nycpivot <mijames@vmware.com>
2022-01-21 12:42:05 -0500

    Renamed file.

--- tap-operations/scripts-initialize.sh
+++
@@ -1,3 +0,0 @@
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tanzu-application-platform/scripts-download.sh
-sh ~/scripts-download.sh
-rm ~/scripts-download.sh
---
+++ tap-operations/scripts-initializer.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tap-operations/scripts-downloader.sh
+sh ~/scripts-downloader.sh
+rm ~/scripts-downloader.sh
