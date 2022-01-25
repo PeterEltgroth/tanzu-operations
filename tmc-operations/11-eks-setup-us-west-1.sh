4da52bfdbd75b25cd98134341983940b432533e9
nycpivot <mijames@vmware.com>
2022-01-24 20:21:41 -0500

    Ongoing changes.

--- tmc-operations/11-eks-setup-us-west-1.sh
+++ tmc-operations/11-eks-setup-us-west-1.sh
@@ -1,6 +1,5 @@
 aws_region_code=us-west-1
 access_cluster_group=access-cluster-group
-quota_cluster_group=quota-cluster-group
 custom_cluster_group=custom-cluster-group
 
 
@@ -17,27 +16,6 @@ aws eks wait cluster-active --name $access_cluster_group
 aws eks create-nodegroup \
         --cluster-name $access_cluster_group \
         --nodegroup-name "${access_cluster_group}-node-group" \
-        --disk-size 500 \
-        --scaling-config minSize=1,maxSize=1,desiredSize=1 \
-        --subnets "subnet-0c277f0344e18e39b" "subnet-0475a32ab6d3501d6" \
-        --instance-types t3a.2xlarge \
-        --node-role arn:aws:iam::964978768106:role/vmware-nodegroup-role \
-        --kubernetes-version 1.21
-
-
-#TMC-QUOTA-CLUSTER
-aws eks create-cluster \
-   --region $aws_region_code \
-   --name $quota_cluster_group \
-   --kubernetes-version 1.21 \
-   --role-arn arn:aws:iam::964978768106:role/vmware-eks-role \
-   --resources-vpc-config subnetIds=subnet-0c277f0344e18e39b,subnet-0475a32ab6d3501d6
-
-aws eks wait cluster-active --name $quota_cluster_group
-
-aws eks create-nodegroup \
-        --cluster-name $quota_cluster_group \
-        --nodegroup-name "${quota_cluster_group}-node-group" \
         --disk-size 500 \
         --scaling-config minSize=1,maxSize=1,desiredSize=1 \
         --subnets "subnet-0c277f0344e18e39b" "subnet-0475a32ab6d3501d6" \
--- tmc-operations/scripts-downloader.sh
+++ tmc-operations/scripts-downloader.sh
@@ -6,7 +6,7 @@ wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operat
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/10-pre-reqs.sh -O tmc/10-pre-reqs.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/11-eks-setup-us-west-1.sh -O tmc/11-eks-setup-us-west-1.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/12-aks-setup-tanzu-operations.sh -O tmc/12-aks-setup-tanzu-operations.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/13-gke-setup-us-east1.sh -O tmc/13-gke-setup-us-east1.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/13-gke-setup-us-west1.sh -O tmc/13-gke-setup-us-west1.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/20-tmc-setup.sh -O tmc/20-tmc-setup.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/100-demo-pre-reqs-workspace.sh -O tmc/100-demo-pre-reqs-workspace.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/101-demo-registry-policy.sh -O tmc/101-demo-registry-policy.sh
