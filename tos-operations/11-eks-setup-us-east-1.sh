a5cf0b23beb17cd79ac6c912f315aafe65b50b99
nycpivot <mijames@vmware.com>
2022-01-26 17:15:20 -0500

    Added tos-operations folder.

--- tos-operations/11-eks-setup-us-east-1.sh
+++ tos-operations/11-eks-setup-us-east-1.sh
@@ -8,7 +8,7 @@ aws eks create-cluster \
    --name $tos_tracing_cluster \
    --kubernetes-version 1.21 \
    --role-arn arn:aws:iam::964978768106:role/vmware-eks-role \
-   --resources-vpc-config subnetIds=subnet-0153c75f91ed045a8,subnet-0bc27bb89b888e579,subnet-04b9bef1f2600ea20,subnet-054820597f34b428a,subnet-013511b2897332ad0,subnet-0a98e6bcbe7f1ad5a
+   --resources-vpc-config subnetIds=subnet-054820597f34b428a #,subnet-0153c75f91ed045a8,subnet-04b9bef1f2600ea20,subnet-013511b2897332ad0,subnet-0a98e6bcbe7f1ad5a,subnet-0bc27bb89b888e579
 
 aws eks wait cluster-active --name $tos_tracing_cluster
 
@@ -17,10 +17,11 @@ aws eks create-nodegroup \
         --nodegroup-name "${tos_tracing_cluster}-node-group" \
         --disk-size 500 \
         --scaling-config minSize=2,maxSize=2,desiredSize=2 \
-        --subnets "subnet-0153c75f91ed045a8" "subnet-0bc27bb89b888e579" "04b9bef1f2600ea20" "054820597f34b428a" "054820597f34b428a" "013511b2897332ad0" "0a98e6bcbe7f1ad5a" \
         --instance-types t3a.2xlarge \
         --node-role arn:aws:iam::964978768106:role/vmware-nodegroup-role \
         --kubernetes-version 1.21
+		--subnets "subnet-054820597f34b428a" # "subnet-0153c75f91ed045a8" "subnet-04b9bef1f2600ea20" "subnet-013511b2897332ad0" "subnet-0a98e6bcbe7f1ad5a" "subnet-0bc27bb89b888e579"
+
 
 rm .kube/config
 
