a0db23044b8ced0f72a474aec8dcdc7ce645e012
nycpivot <mijames@vmware.com>
2022-01-31 22:48:03 -0500

    Added tos integrations.

--- tmc-operations/21-add-wavefront-plugins.sh
+++ tmc-operations/21-add-wavefront-plugins.sh
@@ -2,10 +2,8 @@ tmc_custom_cluster=tmc-custom-cluster
 
 wavefront_token=$(az keyvault secret show --name wavefront-token --subscription nycpivot --vault-name tanzuvault --query value --output tsv)
 
-pe "kubectl config use-context ${tmc_custom_cluster}"
-echo
+kubectl config use-context ${tmc_custom_cluster}
 
-pe "helm repo add wavefront https://wavefronthq.github.io/helm/ && helm repo update"
-echo
+helm repo add wavefront https://wavefronthq.github.io/helm/ && helm repo update
 
-pe "kubectl create namespace wavefront && helm install wavefront wavefront/wavefront --set wavefront.url=https://vmware.wavefront.com --set wavefront.token=${wavefront_token} --set clusterName=${cluster_name} --namespace wavefront"
+kubectl create namespace wavefront && helm install wavefront wavefront/wavefront --set wavefront.url=https://vmware.wavefront.com --set wavefront.token=${wavefront_token} --set clusterName=${tmc_custom_cluster} --namespace wavefront
--- tmc-operations/scripts-downloader.sh
+++ tmc-operations/scripts-downloader.sh
@@ -11,11 +11,11 @@ wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operat
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/21-add-wavefront-plugins.sh -O tmc/21-add-wavefront-plugins.sh
 
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/100-demo-pre-reqs.sh -O tmc/100-demo-pre-reqs.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/101-demo-registry-policy.sh -O tmc/101-demo-registry-policy.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/102-demo-network-policy.sh -O tmc/102-demo-network-policy.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/103-demo-security-policy.sh -O tmc/103-demo-security-policy.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/104-demo-quota-policy-gke.sh -O tmc/104-demo-quota-policy-gke.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/105-demo-quota-policy-aks.sh -O tmc/105-demo-quota-policy-aks.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/101-demo-security-policy.sh -O tmc/101-demo-security-policy.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/102-demo-quota-policy-gke.sh -O tmc/102-demo-quota-policy-gke.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/103-demo-quota-policy-aks.sh -O tmc/103-demo-quota-policy-aks.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/104-demo-registry-policy.sh -O tmc/104-demo-registry-policy.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/105-demo-network-policy.sh -O tmc/105-demo-network-policy.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/106-demo-access-policy.sh -O tmc/106-demo-access-policy.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/107-demo-custom-policy.sh -O tmc/107-demo-custom-policy.sh
 
