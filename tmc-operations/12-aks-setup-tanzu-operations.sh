782cd69e95b10574a74ec35cb4d24a6c4b1d6ed1
nycpivot <mijames@vmware.com>
2022-01-31 21:46:52 -0500

    Added tos integrations.

--- tmc-operations/12-aks-setup-tanzu-operations.sh
+++ tmc-operations/12-aks-setup-tanzu-operations.sh
@@ -1,7 +1,7 @@
 tmc_security_cluster=tmc-security-cluster
 tmc_quota_cluster_aks=tmc-quota-cluster-aks
 
-#TMC-REGISTRY-AND-NETWORK-WORKSPACE-CLUSTER
+#TMC-SECURITY-CLUSTER
 az aks create --name $tmc_security_cluster --resource-group tanzu-mission-control --node-count 1 \
 	--enable-addons monitoring --network-plugin kubenet --network-policy calico --generate-ssh-keys
 	
