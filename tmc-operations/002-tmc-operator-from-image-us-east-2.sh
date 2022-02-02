cf36b37af43615eae5d1fb192aa09ab280fe7298
nycpivot <mijames@vmware.com>
2022-02-02 12:40:19 -0500

    Adding snapshot functionality.

--- tmc-operations/002-tmc-operator-from-image-us-east-2.sh
+++ tmc-operations/002-tmc-operator-from-image-us-east-2.sh
@@ -1,4 +1,6 @@
-image_id=$(aws ec2 describe-images --region us-east-2 --owners self --output text --query 'Images[*].[ImageId]')
+aws ec2 describe-images --region us-east-2 --owners self --output text --query 'Images[*].[ImageId]'
+
+read -p "Input image: " image_Id
 
 instance_id=$(aws ec2 run-instances --image-id $image_id --instance-type t3a.2xlarge --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeSize": 500}}' --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=tmc-operator-us-east-2}]' --security-group-ids tanzu-operations-us-east-2 --key-name tanzu-operations-us-east-2 --output text --query "Instances[0].InstanceId")
 
