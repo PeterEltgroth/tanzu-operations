5163b9fcaf3f6b5eeebb1da72cb55b01c0720830
nycpivot <mijames@vmware.com>
2022-02-01 20:01:24 -0500

    Added tos integrations.

--- tmc-operations/11-create-snapshot.sh
+++ tmc-operations/11-create-snapshot.sh
@@ -1,3 +1,3 @@
-volume_id=$(cat volume.id)
+volume_id=$(cat volume-id.txt)
 
 aws ec2 create-snapshot --volume-id $volume_id
--- tmc-operations/tmc-operator-us-east-2.sh
+++ tmc-operations/tmc-operator-us-east-2.sh
@@ -1,1 +1,1 @@
-ssh ubuntu@ec2-18-191-159-54.us-east-2.compute.amazonaws.com -i ../keys/tanzu-operations-us-east-2.pem
+ssh ubuntu@ec2-3-144-99-16.us-east-2.compute.amazonaws.com -i ../keys/tanzu-operations-us-east-2.pem
