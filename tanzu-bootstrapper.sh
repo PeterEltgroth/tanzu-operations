676fbdf6f2059071dfcc42f699ffb152795eaeda
nycpivot <mijames@vmware.com>
2022-02-03 12:07:43 -0500

    File changes.

--- tanzu-bootstrapper.sh
+++ tanzu-bootstrapper.sh
@@ -7,5 +7,5 @@ fi
 
 if [[ $aws_region_code = "us-east-2" ]]
 then
-    ssh ubuntu@ec2-18-189-32-145.us-east-2.compute.amazonaws.com -i ../keys/tanzu-operations-${aws_region_code}.pem
+    ssh ubuntu@ec2-18-189-32-145.us-east-2.compute.amazonaws.com -i keys/tanzu-operations-${aws_region_code}.pem
 fi
