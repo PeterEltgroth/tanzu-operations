3d9aed5f3c073deaee6ba43e9df64279b7e269d1
nycpivot <mijames@vmware.com>
2022-02-03 12:13:27 -0500

    File changes.

--- scripts-downloader.sh
+++ scripts-downloader.sh
@@ -1,2 +1,3 @@
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/01-tanzu-bootstrapper-setup.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/02-tanzu-operator-from-new.sh
+
