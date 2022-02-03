35a86c8bf86b7f75bc9e1387b08b6de132290385
nycpivot <mijames@vmware.com>
2022-02-03 12:40:06 -0500

    File changes.

--- 02-tanzu-operator-from-new.sh
+++ 02-tanzu-operator-from-new.sh
@@ -2,6 +2,8 @@ read -p "Operator Name: " operator_name
 read -p "AWS Region Code (us-east-2): " aws_region_code
 read -p "Initializer Url: " initializer_url
 
+instance_id=''
+
 if [[ -z $aws_region_code ]]
 then
     aws_region_code=us-east-2
@@ -9,7 +11,9 @@ fi
 
 if [[ $aws_region_code = "us-east-2" ]]
 then
-    instance_id=$(aws ec2 run-instances --image-id ami-0fb653ca2d3203ac1 --instance-type t3a.2xlarge --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeSize": 500}}' --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$\{operator_name}}]' --security-group-ids tanzu-operations-${aws_region_code} --key-name tanzu-operations-${aws_region_code} --user-data '${initializer_url}' --output text --query "Instances[0].InstanceId")
+	operator_name=${operator_name}-${aws_region_code}
+	key_name="ResourceType=instance,Tags=[{Key=Name,Value="${operator_name}"}]"
+    instance_id=$(aws ec2 run-instances --image-id ami-0fb653ca2d3203ac1 --instance-type t3a.2xlarge --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeSize": 500}}' --tag-specifications "${key_name}" --security-group-ids tanzu-operations-${aws_region_code} --key-name tanzu-operations-${aws_region_code} --user-data '${initializer_url}' --output text --query "Instances[0].InstanceId")
 fi
 
 public_dns=$(aws ec2 describe-instances --instance-ids $instance_id | jq "[.Reservations[].Instances[] | { PublicDnsName }]")
--- scripts-downloader.sh
+++ scripts-downloader.sh
@@ -1,3 +1,4 @@
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/01-tanzu-bootstrapper-setup.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/02-tanzu-operator-from-new.sh
 
+
