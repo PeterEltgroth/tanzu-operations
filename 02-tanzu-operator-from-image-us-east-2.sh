8c8b08fd0f7d4a1ad7488d2ab52528c11c079424
nycpivot <mijames@vmware.com>
2022-02-03 13:11:56 -0500

    File changes.

--- 02-tanzu-operator-from-image-us-east-2.sh
+++ 02-tanzu-operator-from-image-us-east-2.sh
@@ -1,17 +1,29 @@
-aws ec2 describe-images --region us-east-2 --owners self --output text --query 'Images[*].[ImageId]'
+read -p "Operator Name: " operator_name
+read -p "AWS Region Code (us-east-2): " aws_region_code
 
-read -p "Input image: " image_Id
+aws ec2 describe-images --region $aws_region_code --owners self --output text --query 'Images[*].[ImageId]'
 
-instance_id=$(aws ec2 run-instances --image-id $image_id --instance-type t3a.2xlarge --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeSize": 500}}' --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=tmc-operator-us-east-2}]' --security-group-ids tanzu-operations-us-east-2 --key-name tanzu-operations-us-east-2 --output text --query "Instances[0].InstanceId")
+read -p "Input image: " image_id
+
+instance_id=''
+
+if [[ -z $aws_region_code ]]
+then
+    aws_region_code=us-east-2
+fi
+
+if [[ $aws_region_code = "us-east-2" ]]
+then
+	operator_name=${operator_name}-${aws_region_code}
+	key_name="ResourceType=instance,Tags=[{Key=Name,Value="${operator_name}"}]"
+	
+	instance_id=$(aws ec2 run-instances --image-id $image_id --instance-type t3a.2xlarge --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeSize": 500}}' --tag-specifications "${key_name}" --security-group-ids tanzu-operations-${aws_region_code} --key-name tanzu-operations-${aws_region_code} --output text --query "Instances[0].InstanceId")
+fi
 
 public_dns=$(aws ec2 describe-instances --instance-ids $instance_id | jq "[.Reservations[].Instances[] | { PublicDnsName }]")
 
 dns=$(eval "echo \${public_dns} | jq '.[] | .PublicDnsName'" | tr -d '"')
 
-aws s3 rm s3://tmc-operations/operator-instance-us-east-2.txt --recursive
-echo $instance_id | aws s3 cp - s3://tmc-operations/operator-instance-us-east-2.txt
 echo
-
 echo "Operator Dns: " $dns
-echo "User Data: http://169.254.169.254/latest/user-data"
 echo
--- 02-tanzu-operator-from-new.sh
+++ 02-tanzu-operator-from-new.sh
@@ -13,6 +13,7 @@ if [[ $aws_region_code = "us-east-2" ]]
 then
 	operator_name=${operator_name}-${aws_region_code}
 	key_name="ResourceType=instance,Tags=[{Key=Name,Value="${operator_name}"}]"
+	
     instance_id=$(aws ec2 run-instances --image-id ami-0fb653ca2d3203ac1 --instance-type t3a.2xlarge --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeSize": 500}}' --tag-specifications "${key_name}" --security-group-ids tanzu-operations-${aws_region_code} --key-name tanzu-operations-${aws_region_code} --user-data "${initializer_url}" --output text --query "Instances[0].InstanceId")
 fi
 
@@ -20,7 +21,9 @@ public_dns=$(aws ec2 describe-instances --instance-ids $instance_id | jq "[.Rese
 
 dns=$(eval "echo \${public_dns} | jq '.[] | .PublicDnsName'" | tr -d '"')
 
+echo
 echo $dns
+echo
 
 #aws s3 rm s3://tmc-operations/operator-instance-us-east-2.txt --recursive
 #echo $instance_id | aws s3 cp - s3://tmc-operations/operator-instance-us-east-2.txt
