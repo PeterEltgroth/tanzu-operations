b6174dd2dab54688d00dd4c62b5ab59bf3c9ce8b
nycpivot <mijames@vmware.com>
2022-02-03 12:59:41 -0500

    File changes.

--- tkg-operations/scripts-downloader.sh
+++ tkg-operations/scripts-downloader.sh
@@ -1,8 +1,6 @@
-rm -rf tmc
-mkdir tmc
+rm -rf tkg
+mkdir tkg
 
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/bootstrap/bootstrapper-setup.sh -O bootstrapper-setup.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/bootstrap/tmc-operator-from-new-us-east-2.sh -O tmc-operator-from-new-us-east-2.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/bootstrap/tmc-operator-from-new-us-west-1.sh -O tmc-operator-from-new-us-west-1.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/bootstrap/tmc-operator-from-image-us-east-2.sh -O tmc-operator-from-image-us-east-2.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/bootstrap/tmc-operator-from-snapshot-us-east-2.sh -O tmc-operator-from-snapshot-us-east-2.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tkg-operations/01-operator-pre-reqs.sh -O tkg/01-operator-pre-reqs.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/02-tanzu-management-cluster-setup-ui.sh -O tkg/02-tanzu-management-cluster-setup-ui.sh
+
