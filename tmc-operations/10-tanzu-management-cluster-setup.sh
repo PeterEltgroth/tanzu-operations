6b4eee570b902f2c8297eec1b89f6930d00be5c3
nycpivot <mijames@vmware.com>
2022-02-04 08:54:37 -0500

    File changes.

--- tmc-operations/10-tanzu-management-cluster-setup.sh
+++ tmc-operations/10-tanzu-management-cluster-setup.sh
@@ -76,7 +76,7 @@ SERVICE_CIDR: 100.64.0.0/13
 TKG_HTTP_PROXY_ENABLED: "false"
 EOF
 
-tanzu management-cluster create $mgmt_cluster_name -f .config/tanzu/tkg/clusterconfigs/tanzu-management-cluster.yaml
+tanzu management-cluster create $mgmt_cluster_name -f .config/tanzu/tkg/clusterconfigs/${mgmt_cluster_name}.yaml
 
 ls .config/tanzu/tkg/clusterconfigs
 
