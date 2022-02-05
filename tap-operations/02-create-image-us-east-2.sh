d4317074d4271dcf53ae0365a596527647ee3384
nycpivot <mijames@vmware.com>
2022-02-05 16:06:19 -0500

    Refactoring bootstrap files.

--- tap-operations/02-create-image-us-east-2.sh
+++ tap-operations/02-create-image-us-east-2.sh
@@ -1,5 +1,11 @@
-aws s3 cp s3://tmc-operations/operator-instance-us-east-2.txt ./
-instance_id=$(cat operator-instance.txt)
+read -p "AWS Region Code: " aws_region_code
 
-aws ec2 create-image --instance-id $instance_id --name "tmc-operator-image-us-east-2" --no-reboot \
-	--tag-specifications 'ResourceType=image,Tags=[{Key=Name,Value=tmc-operator-image-us-east-2}]'
+tap_operator=tap-operator-${aws_region_code}
+tap_operator_image=tap-operator-imsage-${aws_region_code}
+key_name="ResourceType=instance,Tags=[{Key=Name,Value="${tap_operator_image}"}]"
+
+aws s3 cp s3://vmware-tanzu-operations/${tap_operator} ./
+instance_id=$(cat ${tap_operator})
+
+aws ec2 create-image --instance-id $instance_id --name "${tap_operator_image}" --no-reboot \
+	--tag-specifications "${key_name}"
--- tap-operations/tap-operator.sh
+++ tap-operations/tap-operator.sh
@@ -2,5 +2,5 @@ read -p "AWS Region Code: " aws_region_code
 
 if [[ $aws_region_code = "us-west-1" ]]
 then
-    ssh ubuntu@ec2-54-176-237-249.us-west-1.compute.amazonaws.com -i ../keys/tanzu-operations-${aws_region_code}.pem
+    ssh ubuntu@ec2-13-56-189-67.us-west-1.compute.amazonaws.com -i ../keys/tanzu-operations-${aws_region_code}.pem
 fi
