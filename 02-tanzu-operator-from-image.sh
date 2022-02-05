243e7c4b7a811a8377ccf435a2fb2a589aa6b679
nycpivot <mijames@vmware.com>
2022-02-05 12:18:54 -0500

    Refactoring tap scripts.

--- 02-tanzu-operator-from-image.sh
+++ 02-tanzu-operator-from-image.sh
@@ -9,15 +9,16 @@ instance_id=''
 operator_name=${operator_name}-${aws_region_code}
 key_name="ResourceType=instance,Tags=[{Key=Name,Value="${operator_name}"}]"
 
+export AWS_REGION=$aws_region_code
+
 if [[ $aws_region_code = "us-east-2" ]]
 then
 	instance_id=$(aws ec2 run-instances --image-id $image_id --instance-type t3a.2xlarge --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeSize": 500}}' --tag-specifications "${key_name}" --security-group-ids tanzu-operations-${aws_region_code} --key-name tanzu-operations-${aws_region_code} --output text --query "Instances[0].InstanceId")
-else
+elif [[ $aws_region_code = "us-west-1" ]]
 	instance_id=$(aws ec2 run-instances --image-id $image_id --instance-type t3a.2xlarge --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeSize": 500}}' --tag-specifications "${key_name}" --security-group-ids tanzu-operations-${aws_region_code} --key-name tanzu-operations-${aws_region_code} --output text --query "Instances[0].InstanceId")
 fi
 
 public_dns=$(aws ec2 describe-instances --instance-ids $instance_id | jq "[.Reservations[].Instances[] | { PublicDnsName }]")
-
 dns=$(eval "echo \${public_dns} | jq '.[] | .PublicDnsName'" | tr -d '"')
 
 echo
--- 02-tanzu-operator-from-new.sh
+++ 02-tanzu-operator-from-new.sh
@@ -6,10 +6,12 @@ instance_id=''
 operator_name=${operator_name}-${aws_region_code}
 key_name="ResourceType=instance,Tags=[{Key=Name,Value="${operator_name}"}]"
 
+export AWS_REGION=$aws_region_code
+
 if [[ $aws_region_code = "us-east-2" ]]
 then
     instance_id=$(aws ec2 run-instances --image-id ami-0fb653ca2d3203ac1 --instance-type t3a.2xlarge --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeSize": 500}}' --tag-specifications "${key_name}" --security-group-ids tanzu-operations-${aws_region_code} --key-name tanzu-operations-${aws_region_code} --user-data "${initializer_url}" --output text --query "Instances[0].InstanceId")
-else
+elif [[ $aws_region_code = "us-west-1" ]]
 	instance_id=$(aws ec2 run-instances --image-id ami-01f87c43e618bf8f0 --instance-type t3a.2xlarge --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeSize": 500}}' --tag-specifications "${key_name}" --security-group-ids tanzu-operations-${aws_region_code} --key-name tanzu-operations-${aws_region_code} --user-data "${initializer_url}" --output text --query "Instances[0].InstanceId")
 fi
 
