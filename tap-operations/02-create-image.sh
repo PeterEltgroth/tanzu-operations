a6f326c9b73fcc3fc6c0fa31a454ddbf6e3b25ec
nycpivot <mijames@vmware.com>
2022-02-05 16:33:26 -0500

    Refactoring bootstrap files.

--- tap-operations/02-create-image.sh
+++ tap-operations/02-create-image.sh
@@ -1,11 +1,11 @@
 read -p "AWS Region Code: " aws_region_code
 
+export AWS_REGION=$aws_region_code
+
 tap_operator=tap-operator-${aws_region_code}
 tap_operator_image=tap-operator-imsage-${aws_region_code}
-key_name="ResourceType=instance,Tags=[{Key=Name,Value="${tap_operator_image}"}]"
 
 aws s3 cp s3://vmware-tanzu-operations/${tap_operator} ./
 instance_id=$(cat ${tap_operator})
 
-aws ec2 create-image --instance-id $instance_id --name "${tap_operator_image}" --no-reboot \
-	--tag-specifications "${key_name}"
+aws ec2 create-image --instance-id $instance_id --name "${tap_operator_image}" --no-reboot
--- tap-operations/11-eks-build-setup.sh
+++ tap-operations/11-eks-build-setup.sh
@@ -1,79 +1,34 @@
-read -p "Full Cluster Name: " eks_full_cluster
-read -p "Build Cluster Name: " eks_build_cluster
-read -p "Run Cluster Name: " eks_run_cluster
+read -p "Build Cluster Name: " build_cluster
+read -p "AWS Region Code: " aws_region_code
 
-#TAP-FULL-CLUSTER
-aws eks create-cluster \
-   --name $eks_full_cluster \
-   --region us-west-1 \
-   --kubernetes-version 1.21 \
-   --role-arn arn:aws:iam::964978768106:role/vmware-eks-role \
-   --resources-vpc-config subnetIds=subnet-0c277f0344e18e39b,subnet-0475a32ab6d3501d6
+arn=arn:aws:eks:${aws_region_code}:964978768106:cluster
 
-aws eks wait cluster-active --name $eks_full_cluster
+if [[ $aws_region_code = "us-west-1" ]]
+then
+	aws eks create-cluster \
+	   --name $build_cluster \
+	   --region $aws_region_code \
+	   --kubernetes-version 1.21 \
+	   --role-arn arn:aws:iam::964978768106:role/vmware-eks-role \
+	   --resources-vpc-config subnetIds=subnet-0c277f0344e18e39b,subnet-0475a32ab6d3501d6
 
-aws eks create-nodegroup \
-        --cluster-name $eks_full_cluster \
-        --nodegroup-name "${eks_full_cluster}-node-group" \
-        --disk-size 500 \
-        --scaling-config minSize=3,maxSize=3,desiredSize=3 \
-        --subnets "subnet-0c277f0344e18e39b" "subnet-0475a32ab6d3501d6" \
-        --instance-types t3a.2xlarge \
-        --node-role arn:aws:iam::964978768106:role/vmware-nodegroup-role \
-        --kubernetes-version 1.21
+	aws eks wait cluster-active --name $build_cluster
 
-
-#TAP-BUILD-CLUSTER
-aws eks create-cluster \
-   --name $eks_build_cluster \
-   --region us-west-1 \
-   --kubernetes-version 1.21 \
-   --role-arn arn:aws:iam::964978768106:role/vmware-eks-role \
-   --resources-vpc-config subnetIds=subnet-0c277f0344e18e39b,subnet-0475a32ab6d3501d6
-
-aws eks wait cluster-active --name $eks_build_cluster
-
-aws eks create-nodegroup \
-        --cluster-name $eks_build_cluster \
-        --nodegroup-name "${eks_build_cluster}-node-group" \
-        --disk-size 500 \
-        --scaling-config minSize=3,maxSize=3,desiredSize=3 \
-        --subnets "subnet-0c277f0344e18e39b" "subnet-0475a32ab6d3501d6" \
-        --instance-types t3a.2xlarge \
-        --node-role arn:aws:iam::964978768106:role/vmware-nodegroup-role \
-        --kubernetes-version 1.21
+	aws eks create-nodegroup \
+			--cluster-name $build_cluster \
+			--nodegroup-name "${eks_build_cluster}-node-group" \
+			--disk-size 500 \
+			--scaling-config minSize=3,maxSize=3,desiredSize=3 \
+			--subnets "subnet-0c277f0344e18e39b" "subnet-0475a32ab6d3501d6" \
+			--instance-types t3a.2xlarge \
+			--node-role arn:aws:iam::964978768106:role/vmware-nodegroup-role \
+			--kubernetes-version 1.21
+fi
 		
-
-#TAP-RUN-CLUSTER
-aws eks create-cluster \
-   --name $eks_run_cluster \
-   --region us-west-1 \
-   --kubernetes-version 1.21 \
-   --role-arn arn:aws:iam::964978768106:role/vmware-eks-role \
-   --resources-vpc-config subnetIds=subnet-0c277f0344e18e39b,subnet-0475a32ab6d3501d6
-
-aws eks wait cluster-active --name $eks_run_cluster
-
-aws eks create-nodegroup \
-        --cluster-name $eks_run_cluster \
-        --nodegroup-name "${eks_run_cluster}-node-group" \
-        --disk-size 500 \
-        --scaling-config minSize=3,maxSize=3,desiredSize=3 \
-        --subnets "subnet-0c277f0344e18e39b" "subnet-0475a32ab6d3501d6" \
-        --instance-types t3a.2xlarge \
-        --node-role arn:aws:iam::964978768106:role/vmware-nodegroup-role \
-        --kubernetes-version 1.21
-		
-aws eks wait nodegroup-active --cluster-name $eks_run_cluster --nodegroup-name ${eks_run_cluster}-node-group
+aws eks wait nodegroup-active --cluster-name $build_cluster --nodegroup-name ${build_cluster}-node-group
 
 rm .kube/config
 
-aws eks update-kubeconfig --name $eks_full_cluster --region us-west-1
-aws eks update-kubeconfig --name $eks_build_cluster --region us-west-1
-aws eks update-kubeconfig --name $eks_run_cluster --region us-west-1
+aws eks update-kubeconfig --name $eks_build_cluster --region $aws_region_code
 
-arn=arn:aws:eks:us-west-1:964978768106:cluster
-kubectl config rename-context ${arn}/${eks_full_cluster} $eks_full_cluster
 kubectl config rename-context ${arn}/${eks_build_cluster} $eks_build_cluster
-kubectl config rename-context ${arn}/${eks_run_cluster} $eks_run_cluster
-
