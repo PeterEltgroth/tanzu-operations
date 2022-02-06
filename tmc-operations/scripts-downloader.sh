c8b7ac2ba3482a8fab6f77de71b42ab927ad02f7
nycpivot <mijames@vmware.com>
2022-02-06 15:49:25 -0500

    Created a script to provision a TKG cluster with the built-in TMC Management Cluster.

--- tmc-operations/scripts-downloader.sh
+++ tmc-operations/scripts-downloader.sh
@@ -5,11 +5,12 @@ wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operat
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/02-create-image-us-east-2.sh -O tmc/02-create-image-us-east-2.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/02-create-snapshot-us-east-2.sh -O tmc/02-create-snapshot-us-east-2.sh
 
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/10-tkg-create-build.sh -O tmc/10-tkg-create-build.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/11-aks-create-development.sh -O tmc/11-aks-create-development.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/12-eks-create-staging.sh -O tmc/12-eks-create-staging.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/13-gke-create-production.sh -O tmc/13-gke-create-production.sh
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/14-tanzu-management-cluster-setup.sh -O tmc/14-tanzu-management-cluster-setup.sh
+
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/10-aks-create-development.sh -O tmc/10-aks-create-development.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/11-eks-create-staging.sh -O tmc/11-eks-create-staging.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/12-gke-create-production.sh -O tmc/12-gke-create-production.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/13-tanzu-management-cluster-setup.sh -O tmc/13-tanzu-management-cluster-setup.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/14-tkg-create-build.sh -O tmc/14-tkg-create-build.sh
 
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/20-provision-clusters.sh -O tmc/20-provision-clusters.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/21-attach-clusters.sh -O tmc/21-attach-clusters.sh
