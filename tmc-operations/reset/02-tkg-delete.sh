b41ba253570cbc9a6bf988e14739218856720bc0
nycpivot <mijames@vmware.com>
2022-02-06 21:06:38 -0500

    Created a script to provision a TKG cluster with the built-in TMC Management Cluster.

--- tmc-operations/reset/02-tkg-delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-tmc cluster list
-
-read -p "Cluster Name: " cluster_name
-
-tmc cluster delete $cluster_name --management-cluster-name aws-hosted --provisioner-name tmc-aws-provisioner
-
-
