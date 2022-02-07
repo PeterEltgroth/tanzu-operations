ebc5f97f10854c6dc86bb150936a9f98af3bda2d
nycpivot <mijames@vmware.com>
2022-02-06 20:58:16 -0500

    Created a script to provision a TKG cluster with the built-in TMC Management Cluster.

--- tmc-operations/reset/01-delete-tmc.sh
+++ tmc-operations/reset/01-delete-tmc.sh
@@ -1,51 +1,51 @@
 #CLUSTER GROUPS
-cluster_group=cluster-group
-quota_group=quota-group
-workspace_group=workspace-group
+development_cluster_group=development-cluster-group
+production_cluster_group=production-cluster-group
 
 #CLUSTERS
-tmc_access_cluster=tmc-access-cluster
-tmc_registry_and_network_cluster=tmc-registry-and-network-cluster
-tmc_security_cluster=tmc-security-cluster
-tmc_quota_cluster_gke=tmc-quota-cluster-gke
-tmc_quota_cluster_aks=tmc-quota-cluster-aks
-tmc_custom_cluster=tmc-custom-cluster
+tmc_build_cluster=tmc-build-cluster
+tmc_staging_cluster=tmc-staging-cluster
+tmc_development_cluster=tmc-development-cluster
+tmc_production_cluster=tmc-production-cluster
 
 #WORKSPACES
-registry_workspace=registry-workspace
-network_workspace=network-workspace
+app_workspace=app-workspace
+web_workspace=web-workspace
 
 #NAMESPACES
-registry=registry
-network=network
-
+production_web=production-web
+production_api=production-api
+production_app=production-app
+production_data=production-data
 
 #DELETE POLICIES
-tmc organization iam remove-binding -u michael.james.mj.mj@gmail.com -r organization.view
-tmc workspace image-policy delete registry-policy --workspace-name $registry_workspace
-tmc workspace network-policy delete network-policy --workspace-name $network_workspace
-tmc clustergroup security-policy delete security-policy --cluster-group-name $cluster_group
-tmc clustergroup namespace-quota-policy delete quota-policy --cluster-group-name $quota_group
+#tmc organization iam remove-binding -u michael.james.mj.mj@gmail.com -r organization.view
+tmc workspace image-policy delete registry-policy --workspace-name $web_workspace
+tmc workspace network-policy delete network-policy --workspace-name $app_workspace
+tmc clustergroup security-policy delete security-policy --cluster-group-name $development_cluster_group
+tmc clustergroup namespace-quota-policy delete quota-policy --cluster-group-name $development_cluster_group
 
 #DELETE NAMESPACES
-tmc cluster namespace delete $registry --cluster-name $tmc_registry_and_network_cluster --management-cluster-name attached --provisioner-name attached
-tmc cluster namespace delete $network --cluster-name $tmc_registry_and_network_cluster --management-cluster-name attached --provisioner-name attached
+tmc cluster namespace delete $production_web --cluster-name $tmc_production_cluster --management-cluster-name attached --provisioner-name attached
+tmc cluster namespace delete $production_api --cluster-name $tmc_production_cluster --management-cluster-name attached --provisioner-name attached
+tmc cluster namespace delete $production_app --cluster-name $tmc_production_cluster --management-cluster-name attached --provisioner-name attached
+tmc cluster namespace delete $production_data --cluster-name $tmc_production_cluster --management-cluster-name attached --provisioner-name attached
 
 #DELETE WORKSPACES
-tmc workspace delete $registry_workspace
-tmc workspace delete $network_workspace
+tmc workspace delete $app_workspace
+tmc workspace delete $web_workspace
 
 #DELETE CLUSTERS
-tmc cluster delete $tmc_access_cluster --management-cluster-name attached --provisioner-name attached
-tmc cluster delete $tmc_registry_and_network_cluster --management-cluster-name attached --provisioner-name attached
-tmc cluster delete $tmc_security_cluster --management-cluster-name attached --provisioner-name attached
-tmc cluster delete $tmc_quota_cluster_gke --management-cluster-name attached --provisioner-name attached
-tmc cluster delete $tmc_quota_cluster_aks --management-cluster-name attached --provisioner-name attached
-tmc cluster delete $tmc_custom_cluster --management-cluster-name attached --provisioner-name attached
+tmc cluster delete $tmc_build_cluster --management-cluster-name attached --provisioner-name attached
+tmc cluster delete $tmc_staging_cluster --management-cluster-name attached --provisioner-name attached
+tmc cluster delete $tmc_development_cluster --management-cluster-name attached --provisioner-name attached
+tmc cluster delete $tmc_production_cluster --management-cluster-name attached --provisioner-name attached
+
 
 read -p "Enter to continue"
 
+
 #DELETE CLUSTER GROUPS
-tmc clustergroup delete $cluster_group
-tmc clustergroup delete $quota_group
-tmc clustergroup delete $workspace_group
+tmc clustergroup delete $development_cluster_group
+tmc clustergroup delete $production_cluster_group
+
