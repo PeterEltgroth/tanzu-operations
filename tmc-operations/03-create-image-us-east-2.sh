9ffe191961bfed6832fb7fc5e1abb2214ce58e49
nycpivot <mijames@vmware.com>
2022-02-11 12:35:26 -0500

    Added different scripts for different TKG versions.

--- tmc-operations/03-create-image-us-east-2.sh
+++ tmc-operations/03-create-image-us-east-2.sh
@@ -1,5 +1,7 @@
-aws s3 cp s3://tmc-operations/operator-instance-us-east-2.txt ./
-instance_id=$(cat operator-instance.txt)
+read -p "Operator Name: " operator_name
+
+aws s3 cp s3://vmware-tanzu-operations/${operator_name} ./
+instance_id=$(cat $operator_name)
 
 aws ec2 create-image --instance-id $instance_id --name "tmc-operator-image-us-east-2" --no-reboot \
 	--tag-specifications 'ResourceType=image,Tags=[{Key=Name,Value=tmc-operator-image-us-east-2}]'
