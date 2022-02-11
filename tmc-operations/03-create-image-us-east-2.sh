1578861214bc02904dd97a551a677c21e64fa341
nycpivot <mijames@vmware.com>
2022-02-11 13:03:24 -0500

    Added different scripts for different TKG versions.

--- tmc-operations/03-create-image-us-east-2.sh
+++ tmc-operations/03-create-image-us-east-2.sh
@@ -1,4 +1,5 @@
 read -p "Operator Name: " operator_name
+read -p "Image Name: " image_name
 read -p "AWS Region Code (us-east-2): " aws_region_code
 
 if [[ -z $aws_region_code ]]
@@ -9,4 +10,4 @@ fi
 aws s3 cp s3://vmware-tanzu-operations/${operator_name}-${aws_region_code} ./
 instance_id=$(cat ${operator_name}-${aws_region_code})
 
-aws ec2 create-image --instance-id $instance_id --name "${operator_name}-${aws_region_code}" --no-reboot
+aws ec2 create-image --instance-id $instance_id --name "${image_name}-${aws_region_code}" --no-reboot
