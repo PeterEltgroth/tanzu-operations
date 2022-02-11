6e9b00eee276c1d29280c606a965a0f736a0a9cb
nycpivot <mijames@vmware.com>
2022-02-11 14:42:31 -0500

    Added different scripts for different TKG versions.

--- 02-tanzu-operator-from-new.sh
+++ 02-tanzu-operator-from-new.sh
@@ -6,16 +6,16 @@ instance_id=''
 operator_name=${operator_name}-${aws_region_code}
 key_name="ResourceType=instance,Tags=[{Key=Name,Value="${operator_name}"}]"
 
-initializer_url=$(cat tanzu-bootstrapper-files/${operator_prefix}-bootstrapper)
+bootstrapper_url=$(cat tanzu-bootstrapper-files/${operator_prefix}-bootstrapper)
 
 export AWS_REGION=$aws_region_code
 
 if [[ $aws_region_code = "us-east-2" ]]
 then
-    instance_id=$(aws ec2 run-instances --image-id ami-0fb653ca2d3203ac1 --instance-type t3a.2xlarge --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeSize": 500}}' --tag-specifications "${key_name}" --security-group-ids tanzu-operations-${aws_region_code} --key-name tanzu-operations-${aws_region_code} --user-data "${initializer_url}" --output text --query "Instances[0].InstanceId")
+    instance_id=$(aws ec2 run-instances --image-id ami-0fb653ca2d3203ac1 --instance-type t3a.2xlarge --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeSize": 500}}' --tag-specifications "${key_name}" --security-group-ids tanzu-operations-${aws_region_code} --key-name tanzu-operations-${aws_region_code} --user-data "${bootstrapper_url}" --output text --query "Instances[0].InstanceId")
 elif [[ $aws_region_code = "us-west-1" ]]
 then
-	instance_id=$(aws ec2 run-instances --image-id ami-01f87c43e618bf8f0 --instance-type t3a.2xlarge --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeSize": 500}}' --tag-specifications "${key_name}" --security-group-ids tanzu-operations-${aws_region_code} --key-name tanzu-operations-${aws_region_code} --user-data "${initializer_url}" --output text --query "Instances[0].InstanceId")
+	instance_id=$(aws ec2 run-instances --image-id ami-01f87c43e618bf8f0 --instance-type t3a.2xlarge --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeSize": 500}}' --tag-specifications "${key_name}" --security-group-ids tanzu-operations-${aws_region_code} --key-name tanzu-operations-${aws_region_code} --user-data "${bootstrapper_url}" --output text --query "Instances[0].InstanceId")
 fi
 
 public_dns=$(aws ec2 describe-instances --instance-ids $instance_id | jq "[.Reservations[].Instances[] | { PublicDnsName }]")
