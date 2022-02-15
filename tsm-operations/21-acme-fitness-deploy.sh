c0f96b516e463fc729d5843a45ba0f699ff805c4
nycpivot <mijames@vmware.com>
2022-02-15 15:36:57 -0500

    Added acme fitness demo.

--- tsm-operations/21-acme-fitness-deploy.sh
+++ tsm-operations/21-acme-fitness-deploy.sh
@@ -1,10 +1,13 @@
+rm -rf acme-fitness-demo
+
 git clone https://github.com/nycpivot/acme-fitness-demo.git
+
 
 #WEB FRONTEND
 kubectl config use-context nyc-fitness-web
-kubectl apply -f acme_fitness_demo/istio-manifests/gateway.yaml
-kubectl apply -f acme_fitness_demo/kubernetes-manifests/secrets.yaml
-kubectl apply -f acme_fitness_demo/kubernetes-manifests/acme_fitness_cluster1.yaml
+kubectl apply -f acme-fitness-demo/istio-manifests/gateway.yaml
+kubectl apply -f acme-fitness-demo/kubernetes-manifests/secrets.yaml
+kubectl apply -f acme-fitness-demo/kubernetes-manifests/acme_fitness_cluster1.yaml
 
 watch kubectl get pods
 
@@ -13,9 +16,9 @@ kubectl get services -n istio-system | grep istio-ingressgateway
 
 #CATALOG SERVICE
 kubectl config use-context nyc-fitness-catalog
-kubectl apply -f acme_fitness_demo/istio-manifests/gateway.yaml
-kubectl apply -f acme_fitness_demo/kubernetes-manifests/secrets.yaml
-kubectl apply -f acme_fitness_demo/kubernetes-manifests/acme_fitness_cluster2.yaml
+kubectl apply -f acme-fitness-demo/istio-manifests/gateway.yaml
+kubectl apply -f acme-fitness-demo/kubernetes-manifests/secrets.yaml
+kubectl apply -f acme-fitness-demo/kubernetes-manifests/acme_fitness_cluster2.yaml
 
 watch kubectl get pods
 
