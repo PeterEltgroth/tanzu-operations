ee9748e9524663a5fe69347465d6f3bdf9c2fba2
nycpivot <mijames@vmware.com>
2022-02-16 15:48:31 -0500

    Updated tkg-operations.

---
+++ tkg-operations/03-create-snapshot-us-east-2.sh
@@ -0,0 +1,8 @@
+#GET INSTANCE ID SAVED TO S3 FROM PRE-REQS SCRIPTS
+aws s3 cp s3://tkg-operations/operator-instance.txt ./
+instance_id=$(cat operator-instance.txt)
+
+ebs_volume_id=$(aws ec2 describe-instances --instance-ids $instance_id | jq '[.Reservations[].Instances[] | (.BlockDeviceMappings[] | { VolumeId: .Ebs.VolumeId })]')
+volume_id=$(eval "echo \${ebs_volume_id} | jq '.[] | .VolumeId'" | tr -d '"')
+
+aws ec2 create-snapshot --volume-id $volume_id --tag-specifications 'ResourceType=snapshot,Tags=[{Key=Name,Value=tkg-operator-snapshot-us-east-2}]'
