7b7053502c2d734fe020e02a3d0801b2605b92eb
nycpivot <mijames@vmware.com>
2022-02-17 14:04:37 -0500

    Added tbs-operations.

--- tbs-operations/02-install-tbs-v1.4.sh
+++ tbs-operations/02-install-tbs-v1.4.sh
@@ -10,8 +10,8 @@ curl -i -H "Accept: application/json" -H "Content-Type: application/json" -H "Au
 
 
 mkdir $HOME/tanzu-cluster-essentials
-wget https://network.tanzu.vmware.com/api/v2/products/kapp/releases/1010353/product_files/1103926/download --header="Authorization: Bearer ${access_token}" -O $HOME/tanzu-cluster-essentials/tanzu-cluster-essentials-linux-amd64-1.0.0.tgz
-tar -xvf $HOME/tanzu-cluster-essentials/tanzu-cluster-essentials-linux-amd64-1.0.0.tgz -C $HOME/tanzu-cluster-essentials
+wget https://network.tanzu.vmware.com/api/v2/products/kapp/releases/1010353/product_files/1103926/download --header="Authorization: Bearer ${access_token}"
+#tar -xvf $HOME/tanzu-cluster-essentials/tanzu-cluster-essentials-linux-amd64-1.0.0.tgz -C $HOME/tanzu-cluster-essentials
 
 
 
@@ -22,43 +22,5 @@ tar -xvf $HOME/tanzu-cluster-essentials/tanzu-cluster-essentials-linux-amd64-1.0
 
 
 
-mkdir tanzu
-cd tanzu
 
-wget https://tanzustorage.blob.core.windows.net/tanzu/tanzu-cli-bundle-linux-amd64-v1.4.tar
-tar -xvf tanzu-cli-bundle-linux-amd64-v1.4.tar
-rm tanzu-cli-bundle-linux-amd64-v1.4.tar
-
-sudo install cli/core/v1.4.0/tanzu-core-linux_amd64 /usr/local/bin/tanzu
-tanzu version
-
-tanzu plugin install --local cli all
-tanzu plugin list
-
-
-#CARVEL (YTT, KAPP, KBLD, IMGPKG)
-gunzip cli/ytt-linux-amd64-v0.34.0+vmware.1.gz
-chmod ugo+x cli/ytt-linux-amd64-v0.34.0+vmware.1
-sudo mv cli/ytt-linux-amd64-v0.34.0+vmware.1 /usr/local/bin/ytt
-
-gunzip cli/kapp-linux-amd64-v0.37.0+vmware.1.gz
-chmod ugo+x cli/kapp-linux-amd64-v0.37.0+vmware.1
-sudo mv cli/kapp-linux-amd64-v0.37.0+vmware.1 /usr/local/bin/kapp
-
-gunzip cli/kbld-linux-amd64-v0.30.0+vmware.1.gz
-chmod ugo+x cli/kbld-linux-amd64-v0.30.0+vmware.1
-sudo mv cli/kbld-linux-amd64-v0.30.0+vmware.1 /usr/local/bin/kbld
-
-gunzip cli/imgpkg-linux-amd64-v0.10.0+vmware.1.gz
-chmod ugo+x cli/imgpkg-linux-amd64-v0.10.0+vmware.1
-sudo mv cli/imgpkg-linux-amd64-v0.10.0+vmware.1 /usr/local/bin/imgpkg
-
-cd $HOME
-
-#VELERO
-wget https://tanzustorage.blob.core.windows.net/tanzu/velero-linux-v1.6.2_vmware.1.gz
-gzip -d velero-linux-v1.6.2_vmware.1.gz
-
-sudo install velero-linux-v1.6.2_vmware.1 /usr/local/bin/velero
-rm velero-linux-v1.6.2_vmware.1
 
--- tbs-operations/scripts-downloader.sh
+++ tbs-operations/scripts-downloader.sh
@@ -2,5 +2,6 @@ rm -rf tbs
 mkdir tbs
 
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tbs-operations/01-operator-pre-reqs.sh -O tbs/01-operator-pre-reqs.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tbs-operations/02-install-tbs-v1.4.sh -O tbs/02-install-tbs-v1.4.sh
 
 
