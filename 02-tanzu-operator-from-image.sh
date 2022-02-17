1616b12f748a62386e25791db55b18614f69cdcc
nycpivot <mijames@vmware.com>
2022-02-16 21:41:05 -0500

    Updated tkg-operations.

--- 02-tanzu-operator-from-image.sh
+++ 02-tanzu-operator-from-image.sh
@@ -22,6 +22,10 @@ fi
 public_dns=$(aws ec2 describe-instances --instance-ids $instance_id | jq "[.Reservations[].Instances[] | { PublicDnsName }]")
 dns=$(eval "echo \${public_dns} | jq '.[] | .PublicDnsName'" | tr -d '"')
 
+aws s3 rm s3://vmware-tanzu-operations/${operator_name} --recursive
+echo $instance_id | aws s3 cp - s3://vmware-tanzu-operations/${operator_name}
+echo
+
 echo
 echo "OPERATOR DNS:"
 echo $dns
