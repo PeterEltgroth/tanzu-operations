c2e55c822e008a4306e18ad1ae5e9211c7005ea5
nycpivot <mijames@vmware.com>
2022-02-17 13:59:12 -0500

    Added tbs-operations.

--- tbs-operations/scripts-bootstrapper.sh
+++ tbs-operations/scripts-bootstrapper.sh
@@ -1,3 +1,3 @@
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tkg-operations/scripts-downloader.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tbs-operations/scripts-downloader.sh
 sh ~/scripts-downloader.sh
 rm ~/scripts-downloader.sh
