a3f3d6098de55acf409c40fa1271d58f81e1799a
nycpivot <mijames@vmware.com>
2022-02-23 14:08:30 -0500

    Fixed issues.

--- tbs-operations/scripts-downloader.sh
+++ tbs-operations/scripts-downloader.sh
@@ -4,7 +4,8 @@ mkdir tbs
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tbs-operations/01-operator-pre-reqs.sh -O tbs/01-operator-pre-reqs.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tbs-operations/02-tbs-prereqs-v1.4.sh -O tbs/02-tbs-prereqs-v1.4.sh
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tbs-operations/03-tbs-install-v1.4.sh -O tbs/03-tbs-install-v1.4.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tbs-operations/04-kp-secrets-create.sh -O tbs/04-kp-secrets-create.sh
 
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tbs-operations/10-create-image.sh -O tbs/10-create-image.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tbs-operations/100-demo-image-create.sh -O tbs/100-demo-image-create.sh
 
 
