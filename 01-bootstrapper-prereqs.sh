4a0991084ea6304927b77f1b17116b041b02c330
nycpivot <mijames@vmware.com>
2022-02-28 11:19:38 -0500

    Added operator-prereqs script.

--- 01-bootstrapper-prereqs.sh
+++ 01-bootstrapper-prereqs.sh
@@ -5,6 +5,17 @@ yes | sudo apt upgrade
 sudo snap install jq
 sudo apt install unzip
 
+cat <<EOF | tee bootstrapper-files.txt
+{
+	prereqs: https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/01-operator-prereqs.sh,
+	tap: https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tap-operations/scripts-bootstrapper.sh,
+	tbs: https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tbs-operations/scripts-bootstrapper.sh,
+	tkg: https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tkg-operations/scripts-bootstrapper.sh,
+	tmc: https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tmc-operations/scripts-bootstrapper.sh,
+	tsm: https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tsm-operations/scripts-bootstrapper.sh
+}
+EOF
+
 #AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
--- tanzu-bootstrapper.sh
+++ tanzu-bootstrapper.sh
@@ -1,1 +1,1 @@
-ssh ubuntu@ec2-54-234-117-231.compute-1.amazonaws.com -i keys/tanzu-operations-us-east-1.pem
+ssh ubuntu@ec2-3-238-74-118.compute-1.amazonaws.com -i keys/tanzu-operations-us-east-1.pem
