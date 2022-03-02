b54fe2a8cc7a38bf1cfad8fe19d2399b532906be
nycpivot <mijames@vmware.com>
2022-03-02 13:55:23 -0500

    Cleaning up tos-operations files.

--- tos-operations/02-tanzu-operator-creator-us-east-1.sh
+++
@@ -1,7 +0,0 @@
-aws ec2 run-instances \
-        --image-id ami-04505e74c0741db8d \
-        --instance-type t3a.2xlarge \
-        --block-device-mappings '{"DeviceName": "/dev/sda1", "Ebs": {"DeleteOnTermination": true, "VolumeSize": 500}}' \
-        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=tos-operator-us-east-1}]' \
-        --security-group-ids tanzu-operator-us-east-1 \
-        --key-name tanzu-operations-us-east-1
--- tos-operations/tanzu-bootstrapper-us-east-1.sh
+++
@@ -1,1 +0,0 @@
-ssh ubuntu@ec2-3-239-99-136.compute-1.amazonaws.com -i ../keys/tanzu-operations-us-east-1.pem
