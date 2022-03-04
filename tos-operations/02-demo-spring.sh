a89017cc72d1f3f3950cef8436b98eb8e8fdb380
nycpivot <mijames@vmware.com>
2022-03-04 17:48:31 -0500

    Added scripts to showcase spring app.

--- tos-operations/02-demo-spring.sh
+++ tos-operations/02-demo-spring.sh
@@ -1,3 +1,31 @@
+#!/bin/bash
+
+########################
+# include the magic
+########################
+. demo-magic.sh
+
+########################
+# Configure the options
+########################
+
+#
+# speed at which to simulate typing. bigger num = faster
+#
+TYPE_SPEED=20
+
+#
+# custom prompt
+#
+# see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
+#
+#DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
+
+# hide the evidence
+clear
+
+DEMO_PROMPT="${GREEN}➜ TOS ${CYAN}\W "
+
 read -p "Repository Name: " repo_name
 
 if [ -z $repo_name ]
@@ -5,7 +33,26 @@ then
 	repo_name=tanzu-spring-petclinic
 fi
 
-cd tos
-rm -rf $repo_name
+pe "cd tos"
+pe "rm -rf ${repo_name}"
+echo
 
-git clone https://github.com/nycpivot/${repo_name}
+pe "git clone https://github.com/nycpivot/${repo_name}"
+echo
+
+pe "cd ${repo_name}"
+echo
+
+pe "cat pom.xml | grep wavefront"
+echo
+
+pe "cd src/main/resources"
+pe "cat application.properties"
+echo
+
+pe "cd ${HOME}/tos/${repo_name}"
+echo
+
+pe "./mvnw spring-boot:run"
+
+cd $HOME
--- tos-operations/scripts-downloader.sh
+++ tos-operations/scripts-downloader.sh
@@ -1,12 +1,6 @@
 rm -rf tos
+
 mkdir tos
-
-mkdir tos/spring-petclinic
-
-wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tos-operations/spring-petclinic/01-spring-petclinic.sh -O tos/spring-petclinic/01-spring-petclinic.sh
-
-
-
 mkdir tos/wavefront-collector-open-telemetry
 
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tos-operations/wavefront-collector-open-telemetry/01-docker-install.sh -O tos/wavefront-collector-open-telemetry/01-docker-install.sh
