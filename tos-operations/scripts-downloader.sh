192ea67f5edd467e10fbf0dc3d3c224e9cbaaded
nycpivot <mijames@vmware.com>
2022-03-04 18:27:15 -0500

    Added scripts to showcase spring app.

--- tos-operations/scripts-downloader.sh
+++ tos-operations/scripts-downloader.sh
@@ -1,6 +1,10 @@
 rm -rf tos
 
 mkdir tos
+
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tos-operations/01-tos-prereqs.sh -O tos/01-tos-prereqs.sh
+wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tos-operations/02-demo-spring.sh -O tos/02-demo-spring.sh
+
 mkdir tos/wavefront-collector-open-telemetry
 
 wget https://raw.githubusercontent.com/nycpivot/tanzu-operations/main/tos-operations/wavefront-collector-open-telemetry/01-docker-install.sh -O tos/wavefront-collector-open-telemetry/01-docker-install.sh
