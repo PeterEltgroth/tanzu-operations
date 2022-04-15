4a9f94101028ebeccc55bb57ba4cd75145581438
nycpivot <mijames@vmware.com>
2022-04-14 23:36:43 -0400

    Added velero backup scripts.

--- tmc-operations/50-tsm-integration-prereqs.sh
+++ tmc-operations/50-tsm-integration-prereqs.sh
@@ -4,7 +4,7 @@ git clone https://github.com/nycpivot/acme-fitness-demo.git
 
 
 #WEB FRONTEND
-kubectl config use-context nyc-fitness-web
+kubectl config use-context tmc-development-cluster
 kubectl apply -f acme-fitness-demo/istio-manifests/gateway.yaml
 kubectl apply -f acme-fitness-demo/kubernetes-manifests/secrets.yaml
 kubectl apply -f acme-fitness-demo/kubernetes-manifests/acme_fitness_cluster1.yaml
@@ -15,7 +15,7 @@ kubectl get services -n istio-system | grep istio-ingressgateway
 
 
 #CATALOG SERVICE
-kubectl config use-context nyc-fitness-catalog
+kubectl config use-context tmc-staging-cluster
 kubectl apply -f acme-fitness-demo/istio-manifests/gateway.yaml
 kubectl apply -f acme-fitness-demo/kubernetes-manifests/secrets.yaml
 kubectl apply -f acme-fitness-demo/kubernetes-manifests/acme_fitness_cluster2.yaml
