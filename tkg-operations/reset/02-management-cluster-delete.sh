623cd0e4376d48343df8af14d074879cfc63e136
nycpivot <mijames@vmware.com>
2022-06-16 18:35:32 -0400

    Changed default region to us-east-1.

--- tkg-operations/reset/02-management-cluster-delete.sh
+++ tkg-operations/reset/02-management-cluster-delete.sh
@@ -1,9 +1,9 @@
 read -p "Management Cluster Name: " mgmt_cluster_name
-read -p "AWS Region Code (us-east-2): " aws_region_code
+read -p "AWS Region Code (us-east-1): " aws_region_code
 
 if [[ -z $aws_region_code ]]
 then
-	aws_region_code=us-east-2
+	aws_region_code=us-east-1
 fi
 
 export AWS_REGION=${aws_region_code}
