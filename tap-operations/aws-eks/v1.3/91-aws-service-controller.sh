595a0fadfb1e4979138f39ab1662a92e47e21a12
nycpivot <mijames@vmware.com>
2022-10-01 15:50:46 -0400

    Ongoing changes.

--- tap-operations/aws-eks/v1.3/91-aws-service-controller.sh
+++ tap-operations/aws-eks/v1.3/91-aws-service-controller.sh
@@ -2,6 +2,7 @@ read -p "AWS Region: " aws_region_code
 read -p "EKS Cluster Name: " cluster_name
 read -p "AWS Service: " service_name
 
+#https://aws-controllers-k8s.github.io/community/docs/user-docs/install/
 export SERVICE=$service_name
 export AWS_REGION=$aws_region_code
 export RELEASE_VERSION=`curl -sL https://api.github.com/repos/aws-controllers-k8s/$SERVICE-controller/releases/latest | grep '"tag_name":' | cut -d'"' -f4`
@@ -14,10 +15,13 @@ helm install --create-namespace -n $ACK_SYSTEM_NAMESPACE ack-$SERVICE-controller
   oci://public.ecr.aws/aws-controllers-k8s/$SERVICE-chart --version=$RELEASE_VERSION --set=aws.region=$AWS_REGION
 
 helm list --namespace $ACK_SYSTEM_NAMESPACE -o yaml
+sleep 5
 
+#STEP 1: Create an OIDC identity provider for your cluster
+#https://aws-controllers-k8s.github.io/community/docs/user-docs/irsa/#step-1-create-an-oidc-identity-provider-for-your-cluster
 eksctl utils associate-iam-oidc-provider --cluster $cluster_name --region us-west-1 --approve
 
-
+#STEP 2: Create an IAM role and policy for your service account
 # Update the service name variables as needed
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 OIDC_PROVIDER=$(aws eks describe-cluster --name $EKS_CLUSTER_NAME --region $AWS_REGION --query "cluster.identity.oidc.issuer" --output text | sed -e "s/^https:\/\///")
@@ -49,7 +53,10 @@ echo "${TRUST_RELATIONSHIP}" > trust.json
 
 ACK_CONTROLLER_IAM_ROLE="ack-${SERVICE}-controller"
 ACK_CONTROLLER_IAM_ROLE_DESCRIPTION="IRSA role for ACK ${SERVICE} controller deployment on EKS cluster using Helm charts"
+
+aws iam delete-role --role-name $ACK_CONTROLLER_IAM_ROLE
 aws iam create-role --role-name "${ACK_CONTROLLER_IAM_ROLE}" --assume-role-policy-document file://trust.json --description "${ACK_CONTROLLER_IAM_ROLE_DESCRIPTION}"
+
 ACK_CONTROLLER_IAM_ROLE_ARN=$(aws iam get-role --role-name=$ACK_CONTROLLER_IAM_ROLE --query Role.Arn --output text)
 
 
@@ -79,7 +86,7 @@ if [ ! -z "$INLINE_POLICY" ]; then
     echo "ok."
 fi
 
-
+#STEP 3: Associate an IAM role to a service account and restart deployment
 kubectl describe serviceaccount/$ACK_K8S_SERVICE_ACCOUNT_NAME -n $ACK_K8S_NAMESPACE
 
 # Annotate the service account with the ARN
@@ -88,12 +95,12 @@ kubectl annotate serviceaccount -n $ACK_K8S_NAMESPACE $ACK_K8S_SERVICE_ACCOUNT_N
 
 # Note the deployment name for ACK service controller from following command
 kubectl get deployments -n $ACK_K8S_NAMESPACE
-
 read -p "Deployment Name: " deployment_name
+
 kubectl -n $ACK_K8S_NAMESPACE rollout restart deployment $deployment_name
 
-
+#STEP 4: VERIFY SETUP
 kubectl get pods -n $ACK_K8S_NAMESPACE
-
 read -p "Pod Name: " pod_name
+
 kubectl describe pod -n $ACK_K8S_NAMESPACE $pod_name | grep "^\s*AWS_"
