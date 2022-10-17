bd3feef9500ab45d160d09ee83b0609212133619
nycpivot <mijames@vmware.com>
2022-10-17 16:55:28 -0400

    Renamed tap-repository-add file.

--- tap-operations/aws-eks/v1.3/21-tap-repository-add.sh
+++ tap-operations/aws-eks/v1.3/21-tap-repository-add.sh
@@ -14,7 +14,7 @@ export INSTALL_REGISTRY_HOSTNAME=${target_registry}.azurecr.io
 export INSTALL_REGISTRY_USERNAME=$target_registry
 export INSTALL_REGISTRY_PASSWORD=$target_registry_secret
 export TARGET_REPOSITORY=build-service
-export TAP_VERSION=1.3.0-build.22
+export TAP_VERSION=1.3.0
 
 imgpkg copy -b registry.tanzu.vmware.com/tanzu-application-platform/tap-packages:${TAP_VERSION} --to-repo ${INSTALL_REGISTRY_HOSTNAME}/${TARGET_REPOSITORY}/tap-packages
 
---
+++ tap-operations/aws-eks/v1.3/23-profile-view-install.sh
@@ -0,0 +1,215 @@
+cluster_name=tap-profile-view
+target_registry=tanzuapplicationplatform
+git_catalog_repository=tanzu-application-platform
+app_domain=apps.tap.nycpivot.com
+
+pivnet_password=$(az keyvault secret show --name pivnet-registry-secret --subscription nycpivot --vault-name tanzuvault --query value --output tsv)
+target_registry_password=$(az keyvault secret show --name tanzu-application-platform-secret --subscription nycpivot --vault-name tanzuvault --query value --output tsv)
+github_token=$(az keyvault secret show --name github-token-nycpivot --subscription nycpivot --vault-name tanzuvault --query value --output tsv)
+
+#export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:82dfaf70656b54dcba0d4def85ccae1578ff27054e7533d08320244af7fb0343
+export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
+export INSTALL_REGISTRY_USERNAME=mjames@pivotal.io
+export INSTALL_REGISTRY_PASSWORD=$pivnet_password
+
+kubectl config use-context $cluster_name
+
+rm tap-gui-viewer-service-account-rbac.yaml
+cat <<EOF | tee tap-gui-viewer-service-account-rbac.yaml
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: tap-gui
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  namespace: tap-gui
+  name: tap-gui-viewer
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: tap-gui-read-k8s
+subjects:
+- kind: ServiceAccount
+  namespace: tap-gui
+  name: tap-gui-viewer
+roleRef:
+  kind: ClusterRole
+  name: k8s-reader
+  apiGroup: rbac.authorization.k8s.io
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: k8s-reader
+rules:
+- apiGroups: ['']
+  resources: ['pods', 'pods/log', 'services', 'configmaps', 'limitranges']
+  verbs: ['get', 'watch', 'list']
+- apiGroups: ['metrics.k8s.io']
+  resources: ['pods']
+  verbs: ['get', 'watch', 'list']
+- apiGroups: ['apps']
+  resources: ['deployments', 'replicasets', 'statefulsets', 'daemonsets']
+  verbs: ['get', 'watch', 'list']
+- apiGroups: ['autoscaling']
+  resources: ['horizontalpodautoscalers']
+  verbs: ['get', 'watch', 'list']
+- apiGroups: ['networking.k8s.io']
+  resources: ['ingresses']
+  verbs: ['get', 'watch', 'list']
+- apiGroups: ['networking.internal.knative.dev']
+  resources: ['serverlessservices']
+  verbs: ['get', 'watch', 'list']
+- apiGroups: [ 'autoscaling.internal.knative.dev' ]
+  resources: [ 'podautoscalers' ]
+  verbs: [ 'get', 'watch', 'list' ]
+- apiGroups: ['serving.knative.dev']
+  resources:
+  - configurations
+  - revisions
+  - routes
+  - services
+  verbs: ['get', 'watch', 'list']
+- apiGroups: ['carto.run']
+  resources:
+  - clusterconfigtemplates
+  - clusterdeliveries
+  - clusterdeploymenttemplates
+  - clusterimagetemplates
+  - clusterruntemplates
+  - clustersourcetemplates
+  - clustersupplychains
+  - clustertemplates
+  - deliverables
+  - runnables
+  - workloads
+  verbs: ['get', 'watch', 'list']
+- apiGroups: ['source.toolkit.fluxcd.io']
+  resources:
+  - gitrepositories
+  verbs: ['get', 'watch', 'list']
+- apiGroups: ['source.apps.tanzu.vmware.com']
+  resources:
+  - imagerepositories
+  - mavenartifacts
+  verbs: ['get', 'watch', 'list']
+- apiGroups: ['conventions.apps.tanzu.vmware.com']
+  resources:
+  - podintents
+  verbs: ['get', 'watch', 'list']
+- apiGroups: ['kpack.io']
+  resources:
+  - images
+  - builds
+  verbs: ['get', 'watch', 'list']
+- apiGroups: ['scanning.apps.tanzu.vmware.com']
+  resources:
+  - sourcescans
+  - imagescans
+  - scanpolicies
+  verbs: ['get', 'watch', 'list']
+- apiGroups: ['tekton.dev']
+  resources:
+  - taskruns
+  - pipelineruns
+  verbs: ['get', 'watch', 'list']
+- apiGroups: ['kappctrl.k14s.io']
+  resources:
+  - apps
+  verbs: ['get', 'watch', 'list']
+- apiGroups: [ 'batch' ]
+  resources: [ 'jobs', 'cronjobs' ]
+  verbs: [ 'get', 'watch', 'list' ]
+EOF
+
+kubectl apply -f tap-gui-viewer-service-account-rbac.yaml
+
+CLUSTER_URL=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
+
+CLUSTER_TOKEN=$(kubectl -n tap-gui get secret $(kubectl -n tap-gui get sa tap-gui-viewer -o=json \
+	| jq -r '.secrets[0].name') -o=json \
+	| jq -r '.data["token"]' \
+	| base64 --decode)
+
+
+#APPEND GUI SETTINGS
+rm tap-values-view.yaml
+cat <<EOF | tee tap-values-view.yaml
+profile: view
+ceip_policy_disclosed: true # Installation fails if this is not set to true. Not a string.
+shared:
+  ingress_domain: "${app_domain}"
+tap_gui:
+  service_type: ClusterIP
+  app_config:
+    app:
+      baseUrl: http://tap-gui.${app_domain}
+    catalog:
+      locations:
+        - type: url
+          target: https://github.com/nycpivot/${git_catalog_repository}/catalog-info.yaml
+    backend:
+        baseUrl: http://tap-gui.${app_domain}
+        cors:
+          origin: http://tap-gui.${app_domain}
+    kubernetes:
+      serviceLocatorMethod:
+        type: 'multiTenant'
+      clusterLocatorMethods:
+        - type: 'config'
+          clusters:
+            - url: $CLUSTER_URL
+              name: $cluster_name
+              authProvider: serviceAccount
+              serviceAccountToken: $CLUSTER_TOKEN
+              skipTLSVerify: true
+contour:
+  infrastructure_provider: aws
+  envoy:
+    service:
+      aws:
+        LBType: nlb
+appliveview:
+  ingressEnabled: true
+  sslDisabled: true
+EOF
+
+tanzu package install tap -p tap.tanzu.vmware.com -v 1.3.0 --values-file tap-values-full.yaml -n tap-install
+tanzu package installed get tap -n tap-install
+tanzu package installed list -A
+
+kubectl get svc -n tanzu-system-ingress
+
+read -p "Tanzu System Ingress IP: " external_ip
+
+nslookup $external_ip
+read -p "IP Address: " ip_address
+
+rm change-batch.json
+cat <<EOF | tee change-batch.json
+{
+    "Comment": "Update IP address.",
+    "Changes": [
+        {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "*.${app_domain}",
+                "Type": "A",
+                "TTL": 60,
+                "ResourceRecords": [
+                    {
+                        "Value": "${ip_address}"
+                    }
+                ]
+            }
+        }
+    ]
+}
+EOF
+
+aws route53 change-resource-record-sets --hosted-zone-id Z0294944QU6R4X4A718M --change-batch file:///$HOME/change-batch.json
+
+echo http://tap-gui.${app_domain}
